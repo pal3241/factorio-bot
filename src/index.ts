da3717575592dfe9614308e355e64cbcf3be967d
pal3241 <kurniadifahri2@gmail.com>
2026-09-24 09:01:55 +0700

    feat: export FactoBot facade and extended types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 export { createBot } from "./client.js";
 export { FactorioError } from "./errors.js";
 export type {
-  ApiResult, AreaQuery, BoundingBox, Bot, BotAction, BotCreateResult, BotDetail, BridgeErrorCode, ChatMessage, ChatSendOptions,
+  ApiResult, AreaQuery, BoundingBox, Bot, BotAction, BotCreateResult, BotDetail, BotInventoryView, BridgeErrorCode, ChatMessage, ChatSendOptions,
   BuildGhostInput, BuildGhostResult, Capabilities, CapabilityLimits, Chunk, DeltaPage, ElectricMeasurement, ElectricNetwork, EntityDetail,
   EntityQuery, EntitySummary, EventRecord, FactorioClientOptions, FactorioErrorCode,
-  ForceQuery, Inventory, ItemStack, JsonObject, JsonValue, LogisticNetwork, Page, PageQuery, Player, PlayerLocation, PlayerRef,
+  ForceQuery, Inventory, InventoryTransferInput, InventoryTransferResult, ItemDropResult, ItemStack, JsonObject, JsonValue, LogisticNetwork, Page, PageQuery, Player, PlayerLocation, PlayerRef,
   Position, ProductionPage, ProductionRecord, Recipe, ResearchPage, ResearchTechnology, ResourcePage,
   ResourcePatch, SharedEntry, SharedWriteOptions, Snapshot, Surface, SpaceAgeCapabilities, SpaceAgeContentCategory,
   SpaceAgeContentPage, SpaceAgeContentQuery, SpaceAgeContentSummary, SpaceAgeEntityDetail, SpaceAgeSnapshot,
@@ -15,4 +15,7 @@ export type {
 export type { FactorioBotClient } from "./client.js";
 
 export { attachVirtualBot, spawnVirtualBot } from "./high-level.js";
-export type { FindNearestResourceOptions, GotoOptions, MineNearestOptions, MineOptions, SpawnBotOptions, VirtualBot } from "./high-level.js";
+export type { ActionWaitOptions, FindEntityOptions, FindNearestResourceOptions, FollowPlayerOptions, GotoOptions, MineNearestOptions, MineOptions, SpawnBotOptions, VirtualBot } from "./high-level.js";
+
+export { FactoBot, createFactoBot } from "./factobot.js";
+export type { CreateFactoBotOptions, FactoBotPlugin } from "./factobot.js";
